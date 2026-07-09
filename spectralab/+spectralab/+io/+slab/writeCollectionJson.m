4c1de6e39e8bbd7206e4b7874b58be174f9f0439
Professor Christer Törnkvist <chto0703@gmail.com>
2026-07-09 15:12:07 +0200

    Prepare v0.5.0 release

--- spectralab/+spectralab/+io/+slab/writeCollectionJson.m
+++ spectralab/+spectralab/+io/+slab/writeCollectionJson.m
@@ -4,7 +4,7 @@ function writeCollectionJson(collection, filename)
 doc = struct();
 doc.format = "spectralab.collection.v1";
 doc.schema_version = 1;
-doc.created_by = "SpectraLab 0.4.0";
+doc.created_by = "SpectraLab 0.5.0";
 doc.saved_at = char(datetime("now", "TimeZone", "local"));
 doc.collection = collection.toStruct();
 
