c5a8693c196f087999940c413c0564a7c0125b7b
Christer Törnkvist <chto0703@gmail.com>
2026-07-29 16:16:03 +0200

    Use canonical report style for information box layout

--- tests/test_report_style.m
+++ tests/test_report_style.m
@@ -14,6 +14,10 @@ verifyEqual(testCase, style.Box.TitleHeight, 22);
 verifyEqual(testCase, style.ResultsTable.VerticalGap, 28);
 verifyEqual(testCase, style.ResultsTable.RowHeight, 18);
 verifyEqual(testCase, style.ResultsTable.SpaceAfter, 8);
+verifyEqual(testCase, style.InformationBox.VerticalGap, 8);
+verifyEqual(testCase, style.InformationBox.RowHeight, 16);
+verifyEqual(testCase, style.InformationBox.SectionGap, 8);
+verifyEqual(testCase, style.InformationBox.BoxPadding, 12);
 end
 
 function testResultsHeightUsesCanonicalStyle(testCase)
@@ -27,6 +31,26 @@ expected = style.Box.TitleHeight + ...
     style.ResultsTable.VerticalGap + ...
     3 * style.ResultsTable.RowHeight + ...
     style.ResultsTable.SpaceAfter;
+
+verifyEqual(testCase, actual, expected);
+end
+
+
+function testInformationBoxHeightUsesCanonicalStyle(testCase)
+style = spectralab.report.internal.createReportStyle();
+
+row = struct("Label","","DisplayText","");
+model = struct( ...
+    "MetadataRows", repmat(row,4,1), ...
+    "ResultRows", repmat(row,3,1));
+
+actual = spectralab.report.internal.estimateInformationBoxHeight(model);
+
+expected = style.Box.TitleHeight + ...
+    2 * style.InformationBox.BoxPadding + ...
+    4 * style.InformationBox.RowHeight + ...
+    style.InformationBox.SectionGap + ...
+    3 * style.InformationBox.RowHeight;
 
 verifyEqual(testCase, actual, expected);
 end
